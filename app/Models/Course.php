450698a65ccc188be63a2119c560da2f136dbbaa
tuplstuisfive <allimahhir@gmail.com>
2025-01-06 20:16:38 +0500

    1.0.1

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -8,6 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Course extends Model
 {
     use HasFactory;
+    protected $guarded = [
+        'id',
+    ];
 
     public function lessons()
     {
--- app/Models/Lesson.php
+++ app/Models/Lesson.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Database\Seeders\CourseSeeder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,4 +10,34 @@ class Lesson extends Model
 {
     /** @use HasFactory<\Database\Factories\LessonFactory> */
     use HasFactory;
+    protected $guarded = [
+        'id',
+    ];
+
+    public function students()
+    {
+        return $this->belongsToMany(Student::class,'registration');
+    }
+
+    public function teacher()
+    {
+        return $this->belongsToMany(Teacher::class);
+    }
+
+    public function course()
+    {
+        return $this->belongsToMany(Course::class);
+    }
+
+    public function season()
+    {
+        return $this->belongsToMany(Season::class);
+    }
+
+    public function location()
+    {
+        return $this->belongsToMany(Location::class);
+    }
+
+
 }
